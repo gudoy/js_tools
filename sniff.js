fd80a25f3b4314b72a0dc3945e6da6ec024a1398
gudoy <doyer.guyllaume@gmail.com>
2012-10-16 11:54:27 +0200

    - [FIXED] replaced the for in loops on arrays (bad) by simple for loops (better)

--- sniff.js
+++ sniff.js
@@ -1,12 +1,13 @@
 var sniff =
 {
-	init: function(){ return this; },
+	init: function(){ return this.all(); },
 	
 	all: function()
 	{
 		var that 		= this,
 			ua 			= navigator.userAgent || 'unknown',
 			classes 	= '',
+			checks 		= ['platform','browser','engine','os','browserVersion'],
 			platforms 	= ['iPhone','iPad','iPod','android','Android','Windows Phone','Windows','BlackBerry','Bada','webOS'],
 			engines 	= {'AppleWebKit':'Webkit','Gecko':'Gecko','Trident':'Trident','MSIE':'Trident','Presto':'Presto','BlackBerry':'Mango','wOSBrowser':'Webkit'},
 			browsers 	= {'Chrome':'Chrome','CriOS':'Chrome','Firefox':'Firefox','Safari':'Safari','Opera':'Opera','IEMobile':'IE Mobile','MSIE':'IE','Dolfin':'Dolfin'}, 
@@ -17,50 +18,43 @@ var sniff =
 				'opera': '.*(Version)\\/([0-9\.]*)\\s?.*',
 				'safari': '.*(Version)\\/([0-9\.]*)\\s?.*',
 				'blackberry': '.*(BlackBerry[a-zA-Z0-9]*)\\/([0-9\\.]*)\\s.*'
-			}
+			};
 
 		// Set Default values
-		for (var k in ['platform','browser','engine','os','browserVersion']){ app[k] = 'unknown' + k.ucfirst(); }
+		for (var i=0, l=checks.length; i<l; i++)	{ var k = checks[i]; app[k] = 'unknown' + k.ucfirst(); }
 			
 		// Look for platform, browser & engines
-		for (var i in platforms)	{ if ( ua.indexOf(platforms[i]) !== -1 ){ app.platform = platforms[i].toLowerCase(); break; } }
-		for (var name in browsers)	{ if ( ua.indexOf(name) !== -1 ){ app.browser 	= browsers[name].toLowerCase().replace(/\s/,''); break; } }
-		for (var name in engines)	{ if ( ua.indexOf(name) !== -1 ){ app.engine 	= engines[name].toLowerCase(); break; } }
-
-//console.log(ua);
-//alert(ua);	
+		for (var i=0, l=platforms.length; i<l; i++) { if ( ua.indexOf(platforms[i]) !== -1 ){ app.platform = platforms[i].toLowerCase(); break; } }
+		for (var name in browsers)					{ if ( ua.indexOf(name) !== -1 ){ app.browser 	= browsers[name].toLowerCase().replace(/\s/,''); break; } }
+		for (var name in engines)					{ if ( ua.indexOf(name) !== -1 ){ app.engine 	= engines[name].toLowerCase(); break; } }
 
 		// Try to get the browser version data
 		if ( app.browser !== 'unknownBrowser' )
 		{
-			var pattern 	= vRegExp[app.browser] || vRegExp['default'].replace('default', app.browser.ucfirst()); 	// Get regex pattern to use 
+			var pattern 	= vRegExp[app.browser] || vRegExp['default'].replace('default', app.browser.ucfirst()), 	// Get regex pattern to use 
 				p 			= ua.replace(new RegExp(pattern, 'gi'), '$2').split('.'); 					// Split on '.'
 
-
-//console.log(pattern);
-//console.log(p);
 				p.unshift(p.join('.') || '?') 	// Insert the full version as the 1st element
 				p.pad(5, '?') 					// Force the parts & default version arrays to have same length
-//console.log(p)
 			
 			// Assoc default version array keys to found values
-			//app.browserVersion = [].combine(version, p);
 			app.browserVersion = {'full': p[0], 'major':p[1], 'minor':p[2], 'build':p[3], 'revision':p[4]};
-			
-alert(app.browserVersion.full);
 		}
+		else { app.browserVersion = version; }
 		
 		// Look for os
 		if 		( ['iphone','ipad','ipod'].inArray(app.platform) )	{ app.os = 'ios'; }
 		else if ( app.platform === 'windows phone' )				{ app.os = 'wpos'; }
 		else if ( app.plafform !== 'unknownPlatform' ) 				{ app.os = app.platform.toLowerCase(); }
 		
+		// Get or test some usefull properties 
 		app.device 			= { 'screen':{w:window.screen.width, h:window.screen.height} };
 		app.isSimulator 	= ua.indexOf('XDeviceEmulator') > -1;
 		app.isStandalone 	= typeof navigator.standalone !== 'undefined' && navigator.standalone;
 		app.isRetina 		= (window.devicePixelRatio && window.devicePixelRatio > 1) || false;
 		app.isMobile 		= ua.indexOf('Mobile') !== -1;
 		
+		// And add retrieven data to classname on the html tag
 		classes = 
 			app.platform + ' ' + app.os + ' ' + app.engine + ' ' + app.browser 
 			+ (app.isStandalone ? ' ' : ' no-') + 'standalone' 
@@ -73,10 +67,14 @@ alert(app.browserVersion.full);
 			'data-os': app.os,
 			'data-browser': app.browser,
 			'data-engine': app.engine,
-			'data-browserVersion': app.browserVersion,
+			'data-browserVersion': JSON.stringify(app.browserVersion) || app.browserVersion,
+			'data-bvMajor': app.browserVersion.major,
+			'data-bvMinor': app.browserVersion.minor,
+			'data-bvBuild': app.browserVersion.build,
+			'data-bvRevision': app.browserVersion.revision
 		})
 		.removeClass('no-js');
 
 		return this;
-	},
+	}
 }
